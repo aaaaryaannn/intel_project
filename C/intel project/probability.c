2d23f57e900ccb595031b315d1eddc965e13563b
aaaaryaannn <91652136+aaaaryaannn@users.noreply.github.com>
2023-03-05 23:41:27 +0530

    Update probability.c

--- C/intel project/probability.c
+++ C/intel project/probability.c
@@ -1,15 +1,15 @@
 include <stdio.h>
 
 int main() {
-    float probability, outcomes, favorable_outcomes;
+    float probability, sample_space, favorable_outcomes;
 
     printf("Enter the number of possible outcomes: ");
-    scanf("%f", &outcomes);
+    scanf("%f", &sample_space);
 
     printf("Enter the number of favorable outcomes: ");
     scanf("%f", &favorable_outcomes);
 
-    probability = favorable_outcomes /outcomes;
+    probability = favorable_outcomes /sample_space;
 
     printf("The probability of the event is: %.2f", probability); //We use the %.2f format specifier to round the result to two decimal places.
 
